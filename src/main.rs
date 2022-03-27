c344bc21306a57807b5fa0f573bb41903d5ebdbf
Tadas Audinis <tadas.audinis@vinted.com>
2022-03-27 18:56:36 +0300

    Add panic_handler attribute and fn

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+
+use core::panic::PanicInfo;
+
+
+// This function is called on panic.
+// ! (never type) means it's a diverging fn - no return.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    // Loop indefinitely
+    loop {}
 }
+
+fn main() {}
